246a653e3bd75c7a75f6a07377b5052c2499a5d1
njitmani <mk2368@njit.edu>
2024-03-29 20:02:53 -0400

    remove unnecessary commenyt

--- App/strategies/statistical_strategies.py
+++ App/strategies/statistical_strategies.py
@@ -1,23 +1,23 @@
 from statistics import mean, median, stdev
-from App.strategies.base_strategy import OperationStrategy  # Corrected the typo in the path
+from App.strategies.base_strategy import OperationStrategy
 from App.utils.wrapper import wrapper
 
 class MeanOperation(OperationStrategy):
     @wrapper
     def execute(self, *operands):
-        self.validate_operands(*operands)  # Validate input before performing the operation
+        self.validate_operands(*operands)
         return mean(operands)
 
 class MedianOperation(OperationStrategy):
     @wrapper
     def execute(self, *operands):
-        self.validate_operands(*operands)  # Validate input before performing the operation
+        self.validate_operands(*operands)
         return median(operands)
 
 class StdDevOperation(OperationStrategy):
     @wrapper
     def execute(self, *operands):
-        self.validate_operands(*operands)  # Validate input before performing the operation
+        self.validate_operands(*operands)
         if len(operands) < 2:
             raise ValueError("Standard deviation requires at least two operands.")
         return stdev(operands)
